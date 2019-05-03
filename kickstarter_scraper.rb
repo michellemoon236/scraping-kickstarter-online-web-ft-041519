41eb19b545cd90cc28cf127dc5dde5ecb12fd7cb
Michelle Moon <michellemoon236@gmail.com>
2019-05-03 22:16:13 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -17,7 +17,13 @@ def create_project_hash
   # percentage_funded: project.css(".first.funded strong").text.gsub("%","").to_i
   
   projects = {}
+  
   kickstarter.css("li.project.grid_4").each do |project|
-    
+    project_name = 
+    projects = { "#{project.css("h2.bbcard_name strong a").text}" => {} }
+    # image_link: project.css("div.project-thumbnail a img").attribute("src").value
+    # description: project.css("p.bbcard_blurb").text
+    # location: project.css(".location-name").text
+    # percentage_funded: project.css(".first.funded strong").text.gsub("%","").to_i
     
 end
