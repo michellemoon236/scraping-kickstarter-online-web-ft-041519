1ef25ce279a137b2adfe674bd0e0b4c790d13c38
Michelle Moon <michellemoon236@gmail.com>
2019-05-03 22:08:36 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,19 @@
-# require libraries/modules here
+require 'nokogiri'
+
+require 'pry'
 
 def create_project_hash
-  # write your code here
+  
+  html = File.read('fixtures/kickstarter.html')
+ 
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
+
+  # projects: kickstarter.css("li.project.grid_4")
+  # title: project.css("h2.bbcard_name strong a").text
+  # image_link: project.css("div.project-thumbnail a img").attribute("src").value
+  # description: project.css("p.bbcard_blurb").text
+  # location: project.css(".location-name").text
+  # percentage_funded: project.css(".first.funded strong").text.gsub("%","").to_i
+  
 end
