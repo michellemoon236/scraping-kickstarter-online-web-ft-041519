c58a544f9fbde227f9e4bcfdfc7c8929d348d72d
Michelle Moon <michellemoon236@gmail.com>
2019-05-03 22:10:17 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -16,4 +16,6 @@ def create_project_hash
   # location: project.css(".location-name").text
   # percentage_funded: project.css(".first.funded strong").text.gsub("%","").to_i
   
+  projects.each do |project|
+    
 end
